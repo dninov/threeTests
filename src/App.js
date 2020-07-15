1b13d96c1dc22800385d72a8b0f3e7b925c0f4d4
unknown <dninov@gmail.com>
2020-07-15 12:06:11 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef} from 'react';
+import './App.scss';
+
+import {Canvas, useFrame} from "react-three-fiber";
+
+const SpinningMesh = ({position, args, color}) =>{
+  const mesh = useRef(null);
+  useFrame (()=>(mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
+  return(
+    <mesh position={position} ref = {mesh}>
+    <boxBufferGeometry attach='geometry' args= {args}/>
+    <meshStandardMaterial attach = 'material'color = {color}/>
+  </mesh>
+  )
+}
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Canvas colorManagement camera = {{position: [-5,2,10], fov:60}}>
+      <ambientLight intensity = {0.3} />
+      <pointLight position = {[-10,0,-20]} intensity={0.3} />
+      <pointLight position = {[0,-10, 0]} intensity={1.5} />
+      <SpinningMesh position={[0,1,0]} args = {[3,2,1]} color = 'lightblue'/>
+      <SpinningMesh position={[-2,1,-5]} color = "pink"/>
+      <SpinningMesh position={[5,1,-2]} color = "pink"/>
+
+    </Canvas>
+  </>
   );
 }
 
